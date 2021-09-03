21d6eaacf0c86401276ca2671ca7ca9bcf7448fa
Viacheslav Skoropupov <vs@keyba.ru>
2021-09-03 17:10:59 +0700

    [#5] - Templates, config, server errors - utils

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module shina
 
 go 1.16
+
+require github.com/spf13/viper v1.8.1 // indirect
---
+++ internal/utils/errors.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"log"
+	"net/http"
+	"runtime/debug"
+)
+
+func ServerError(w http.ResponseWriter, err error) {
+	log.Printf("%s\n%s", err.Error(), debug.Stack())
+	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+}
+
+func ClientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+func NotFound(w http.ResponseWriter) {
+	ClientError(w, http.StatusNotFound)
+}
